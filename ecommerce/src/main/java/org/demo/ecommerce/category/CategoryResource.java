862e04adbab76c725173a6c199697f0a50c618db
neo-lestat <nmtzodev@gmail.com>
2025-02-09 14:53:28 +0100

    Change response status code for create product and category

--- ecommerce/src/main/java/org/demo/ecommerce/category/CategoryResource.java
+++ ecommerce/src/main/java/org/demo/ecommerce/category/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -48,6 +50,7 @@ public class CategoryResource {
     }
 
     @PostMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
     public Category create(@Valid @RequestBody CategoryDto categoryDto) {
         Category category =  categoryMapper.fromCategoryDto(categoryDto);
         return categoryService.create(category);
--- ecommerce/src/main/java/org/demo/ecommerce/product/ProductResource.java
+++ ecommerce/src/main/java/org/demo/ecommerce/product/ProductResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -52,6 +54,7 @@ public class ProductResource {
     }
 
     @PostMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
     public Product create(@Valid @RequestBody ProductDto productDto) {
         Product product = productMapper.fromProductDto(productDto);
         return productService.create(product);
